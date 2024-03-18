018dcaa48ccc22ee83eda52d98ff09e8157a5ae4
ravindra <you@example.com>
2024-03-18 15:38:39 +0530

    testdevops

---
+++ HibernateUtil.java
@@ -0,0 +1,41 @@
+package in.ineuron.util;
+
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+	static Configuration cfg = null;
+	static SessionFactory sessionFactory = null;
+	static Session session = null;
+	static {
+		try {
+			cfg = new Config
+					uration();
+			cfg.configure();
+			sessionFactory = cfg.buildSessionFactory();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Session getSession() {
+		if (session == null)
+			session = sessionFactory.openSession();
+		return session;
+	}
+
+	public static void closeSession(Session session) {
+		if (session != null) {
+			session.close();
+		}
+	}
+
+	public static void closeSessionFactory(SessionFactory sessionFactory) {
+		if (sessionFactory != null) {
+			sessionFactory.close();
+		}
+	}
+
+}
